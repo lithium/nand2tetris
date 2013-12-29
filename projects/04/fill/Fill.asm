c4a463637920b84f36f3627b10a785b425a8c848
lithium <code@hlidskialf.com>
2013-12-29 12:08:00 -0800

    Fill Assembly routine

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,11 +1,53 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+// Keyboard Screen Flash Assembly 
+// For the book "The Elements of Computing Systems" (www.nand2tetris.org)
+// Author: Matthew Wiggins
+
 
 // Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+
+
+(FillScreen)
+    @8191           // count = 8191
+    D=A
+    @count
+    M=D
+
+    @24576          // check keyboard, if zero, erase screen
+    D=M
+    @EraseScreen
+    D;JEQ
+                    // otherwise blacken the screen
+
+(BlackenScreen)
+    @count          // A = screen+count--
+    D=M
+    M=M-1
+    @SCREEN
+    A=D+A
+    M=-1            // *A = 0xffff
+
+    @BlackenScreen  // loop while count >= 0
+    D;JGE
+
+    @FillScreen     // return to main loop
+    0;JMP
+
+(EraseScreen)
+    @count          // A = screen+count--
+    D=M
+    M=M-1
+    @SCREEN
+    A=D+A
+    M=0             // *A = 0
+
+    @EraseScreen    // loop while count >= 0
+    D;JGE           
+
+
+    @FillScreen     // return to main loop
+    0;JMP
+
